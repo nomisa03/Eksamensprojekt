df4ab819dd54fc2f5c2c96d219f176101a8def4b
nomisa03 <60389851+nomisa03@users.noreply.github.com>
2021-04-27 09:08:47 +0200

    Flytter det til en mappe

--- MAin/Main.py
+++ MAin/Main.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter.ttk import *
+from Astar import main
 
 class Mainwindow:
     def __init__(self):
@@ -8,6 +9,8 @@ class Mainwindow:
         hej = Label(self.root, text="Hej")
         hej.pack
 
+        Astar = Button(self.root, Text="Astar", COMMAND=lambda: main(self))
+        Astar.pack
 
 
         mainloop()
