371505a467756aeff490410201c295340d6de728
nomisa03 <60389851+nomisa03@users.noreply.github.com>
2021-05-17 20:59:45 +0200

    Lavet lidt vidre på GUI

--- MAin/Main.py
+++ MAin/Main.py
@@ -3,18 +3,20 @@ from tkinter.ttk import *
 
 #Importer de andre windows
 from Primtal import primtalmenu
+from Fibonaccisekvens import Fibonaci
 
 class Mainwindow:
     def __init__(self):
         self.root = Tk()
         
-        self.root.geometry("600x200")
+        self.root.geometry("400x150")
 
         hej = Label(self.root, text="Hej")
-        hej.pack(padx = 20, pady = 0, side=LEFT)
+        hej.pack(padx = 20, pady = 0, side=TOP)
         
         Prime = Button(self.root, text="primtals menu", command = lambda:primtalmenu(self))
         Prime.pack(padx = 20, pady = 0, side=LEFT)
+
 
 
 
