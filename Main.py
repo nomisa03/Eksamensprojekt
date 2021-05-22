467680eba740bc734426543fa01a41edd52086da
jijihansen <jiji0002.edu@gmail.com>
2021-05-22 13:03:33 +0200

    Ny navngivning for buttons

--- Main.py
+++ Main.py
@@ -14,10 +14,10 @@ class Mainwindow:
         hej = Label(self.root, text="Velkommen til vores algoritme GUI \nHer kan du vælge imellem de to algoritmer: \ngod fornøjelse:)")
         hej.pack(padx = 20, pady = 0, side=TOP)
         
-        Prime = Button(self.root, text="Primtals menu", command = lambda:primtalmenu(self))
+        Prime = Button(self.root, text="Primtal", command = lambda:primtalmenu(self))
         Prime.pack(padx = 20, pady = 0, side=LEFT)
 
-        fibonaci = Button(self.root, text="Fibonaci Menu", command = lambda:Fibonaci(self))
+        fibonaci = Button(self.root, text="Fibonacci", command = lambda:Fibonaci(self))
         fibonaci.pack(padx = 20, pady = 0, side=LEFT)
 
 
